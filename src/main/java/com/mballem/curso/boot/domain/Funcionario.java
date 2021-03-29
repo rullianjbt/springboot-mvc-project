3e8a7c0dbbedaeb45406e328148c4fe88855bdd3
Rullian Siqueira da Mata <rullianjbt@gmail.com>
2021-03-29 18:28:15 -0300

    Created domain classes

---
+++ src/main/java/com/mballem/curso/boot/domain/Funcionario.java
@@ -0,0 +1,88 @@
+package com.mballem.curso.boot.domain;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+@SuppressWarnings("serial")
+@Entity
+@Table(name = "FUNCIONARIOS")
+public class Funcionario extends AbstractEntity<Long> {
+	
+	@Column(nullable = false, unique = true)
+	private String nome;
+	
+	@Column(nullable = false, columnDefinition = "DECIMAL(7,2) DEFAULT 0.00")
+	private BigDecimal salario;
+	
+	@Column(name="data_entrada", nullable = false, unique = true, columnDefinition = "DATE")
+	private LocalDate dataEntrada;
+	
+	@Column(name="data_saida", unique = true, columnDefinition = "DATE")
+	private LocalDate dataSaida;
+	
+	//CascadeType = all >> Ao adicionar, editar ou excluir um funcionário já realiza a respectiva operação com o endereço
+	@OneToOne(cascade = CascadeType.ALL)
+	@JoinColumn(name = "endereco_id_fk")
+	private Endereco endereco;
+	
+	@ManyToOne
+	@JoinColumn(name = "cargo_id_fk")
+	private Cargo cargo;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public BigDecimal getSalario() {
+		return salario;
+	}
+
+	public void setSalario(BigDecimal salario) {
+		this.salario = salario;
+	}
+
+	public LocalDate getDataEntrada() {
+		return dataEntrada;
+	}
+
+	public void setDataEntrada(LocalDate dataEntrada) {
+		this.dataEntrada = dataEntrada;
+	}
+
+	public LocalDate getDataSaida() {
+		return dataSaida;
+	}
+
+	public void setDataSaida(LocalDate dataSaida) {
+		this.dataSaida = dataSaida;
+	}
+
+	public Endereco getEndereco() {
+		return endereco;
+	}
+
+	public void setEndereco(Endereco endereco) {
+		this.endereco = endereco;
+	}
+
+	public Cargo getCargo() {
+		return cargo;
+	}
+
+	public void setCargo(Cargo cargo) {
+		this.cargo = cargo;
+	}
+	
+}
